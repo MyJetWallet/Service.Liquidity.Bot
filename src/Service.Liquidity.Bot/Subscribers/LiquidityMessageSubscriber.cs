a85c9d9dcc3fdec40193b8b964ce064eb500064e
Yuriy Plyaskin <yuriy.p@smplt.net>
2022-02-09 12:57:57 +0300

    Update message type to html

--- src/Service.Liquidity.Bot/Subscribers/LiquidityMessageSubscriber.cs
+++ src/Service.Liquidity.Bot/Subscribers/LiquidityMessageSubscriber.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using MyJetWallet.Sdk.Service;
 using Service.Liquidity.Monitoring.Domain.Models;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 
 namespace Service.Liquidity.Bot.Subscribers
 {
@@ -31,13 +32,13 @@ namespace Service.Liquidity.Bot.Subscribers
             {
                 var uniMessage = message.Message;
                 _logger.LogInformation("Ready to send message {message}", uniMessage);
-                var result = await _botApiClient.SendTextMessageAsync(Program.Settings.ChatId, uniMessage);
+                var result = await _botApiClient.SendTextMessageAsync(Program.Settings.ChatId, 
+                    uniMessage, ParseMode.Html);
                 _logger.LogInformation("Message was sent with id {id}", result.MessageId);
             }
             catch (Exception e)
             {
                 _logger.LogInformation(e, "Error during consumptions {@context}", message);
-                throw;
             }
             _logger.LogInformation("Has been consumed {@context}", message);            
         }
